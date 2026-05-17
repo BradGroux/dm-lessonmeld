c0f75161de6071a13b91daed6d9db850c6d6f0fa
Brad Groux <3053586+BradGroux@users.noreply.github.com>
2026-05-16 20:08:37 -0500

    Update v0.0.4 cask metadata

--- Casks/dm-lessonmeld.rb
+++ Casks/dm-lessonmeld.rb
@@ -1,6 +1,6 @@
 cask "dm-lessonmeld" do
-  version "0.0.3"
-  sha256 "4a57642d95b5c28f3475c62d52f8d540e12f2f0ee813b9e1a3cf87ea2cbb22dc"
+  version "0.0.4"
+  sha256 "e7be57e706d14a7dbdd3c014953eadced6028780c4db9da20db17eee0e1779f8"
 
   url "https://github.com/BradGroux/dm-lessonmeld/releases/download/v#{version}/dm-lessonmeld-#{version}-macos.zip"
   name "Digital Meld LessonMeld"
